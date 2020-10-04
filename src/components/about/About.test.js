43d395490a48b68209d669c634a29dc87009c25e
Chris Skaaning <c@rpediem.com>
2020-10-04 23:48:46 +0100

    Removed local config

---
+++ src/components/about/About.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {MemoryRouter} from 'react-router-dom';
+import { render, act } from '@testing-library/react';
+import PageService from '../../services/PageService';
+import About from './About';
+
+describe('About', () => {
+    let getPageResolve;
+
+    beforeEach(() => {
+        PageService.getPage = jest.fn();
+        PageService.getPage.mockReturnValue(new Promise(resolve => {
+            getPageResolve = resolve;
+        }));
+    });
+
+    it('renders the page', async () => {
+      const { getByText } = render(<About />);
+
+      await act(async () => {
+         await getPageResolve({
+             headline: 'Om mig',
+             content: 'Jeg hedder June og sådan'
+         });
+      });
+
+      expect(getByText('Om mig')).toBeInTheDocument();
+      expect(getByText('Jeg hedder June og sådan')).toBeInTheDocument();
+    });
+});
