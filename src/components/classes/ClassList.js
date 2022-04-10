02a0557ae0a232b1c085d8e6aa19854cadcf053c
Chris Skaaning <cskaaning@digitalocean.com>
2022-04-10 21:57:56 +0100

    Fixing colours and adding addresses for classes

--- src/components/classes/ClassList.js
+++ src/components/classes/ClassList.js
@@ -15,12 +15,21 @@ function ClassList({classes}) {
           }).map((classObject, index) => {
             return (<div className="d-flex mb-2">
               <div className="class-image" style={{backgroundImage: `url(${classObject.imageUrl})`}} />
-              <div className="class p-2" key={`class-${index}`}>
+              <div className="class p-2 w-50" key={`class-${index}`}>
                 <h5>{classObject.name}</h5>
-                <p className="mb-1">Tid: {classObject.time.replace(day, '')}</p>
+                <p className="mb-1">Tid: {classObject.time}</p>
                 <small>Beskrivelse: {classObject.text}</small>
               </div>
-              <a href={`mailto:june@yogalates.dk?subject=Tilmelding til ${classObject.name} - ${classObject.time}`} className="btn btn-sm btn-primary ms-auto me-4 h-25">Tilmeld hold</a>
+              <div className="class-address p-2 w-25">
+                <p className="mb-1">Adresse:</p>
+                <p class="small mb-0">{classObject.address1}</p>
+                <p class="small mb-0">{classObject.address2}</p>
+                <p class="small mb-0">{classObject.city}</p>
+              </div>
+              <div className="class-actions ms-auto me-4 d-flex flex-column">
+                <a href={`mailto:june@yogalates.dk?subject=Tilmelding til ${classObject.name} - ${classObject.time}`} className="btn btn-sm btn-primary">Tilmeld hold</a>
+                {classObject.link && classObject.link !== 'www.yogalates.dk' ? <a href={`http://${classObject.link.replace('http://', '').replace('https://', '')}`} className="btn btn-sm btn-outline-primary mt-1">Hjemmeside</a> : ''}
+              </div>
             </div>);
           })}
         </div>)
